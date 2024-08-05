5f0ba58c50852fd33149cba28af30a9115ca9874
ian <ianvictorandrade@hotmail.com>
2024-08-05 12:24:13 -0400

    refac: ajustes na classe de api do mercado pago

--- src/main/kotlin/com/ifro/tcc_livraria_back/api/ApiMercadoPago.kt
+++ src/main/kotlin/com/ifro/tcc_livraria_back/api/ApiMercadoPago.kt
@@ -1,116 +1,41 @@
 package com.ifro.tcc_livraria_back.api
 
 import com.mercadopago.MercadoPagoConfig
-import com.mercadopago.client.common.AddressRequest
-import com.mercadopago.client.common.IdentificationRequest
-import com.mercadopago.client.common.PhoneRequest
 import com.mercadopago.client.preference.*
+import com.mercadopago.resources.preference.Preference
 import java.math.BigDecimal
 
 class ApiMercadoPago {
 
-    private val mercadoPagoConfig = MercadoPagoConfig.setAccessToken("TEST-1234567890")
+    init {
+        MercadoPagoConfig.setAccessToken("YOUR_ACCESS_TOKEN")
+    }
 
     private val client = PreferenceClient()
 
-    private val itemRequest: PreferenceItemRequest? = PreferenceItemRequest.builder()
-        .id("1234")
-        .title("Dummy Title")
-        .description("Dummy description")
-        .pictureUrl("http://www.myapp.com/myimage.jpg")
-        .categoryId("car_electronics")
+    private val itemRequest: PreferenceItemRequest = PreferenceItemRequest.builder()
+        .id("1")  // Identificador do livro (pode ser qualquer identificador único)
+        .title("O Trono Vazio")  // Título do livro
         .quantity(1)
         .currencyId("BRL")
-        .unitPrice(BigDecimal("10"))
+        .unitPrice(BigDecimal("42.99"))  // Preço do livro
         .build()
 
-    private val items = mutableListOf(itemRequest)
-
-    private val freeMethod: PreferenceFreeMethodRequest? = PreferenceFreeMethodRequest.builder()
-        .id(1L).build()
-    private val freeMethodList = mutableListOf(freeMethod)
-
-    private val excludedPaymentTypes = mutableListOf(
-        PreferencePaymentTypeRequest.builder().id("ticket").build()
-    )
-
-    private val excludedPaymentMethods = mutableListOf(
-        PreferencePaymentMethodRequest.builder().id("").build()
-    )
-
-    private val preferenceRequest: PreferenceRequest? = PreferenceRequest.builder()
+    private val preferenceRequest: PreferenceRequest = PreferenceRequest.builder()
+        .items(listOf(itemRequest))
         .backUrls(
             PreferenceBackUrlsRequest.builder()
-                .success("http://test.com/success")
-                .failure("http://test.com/failure")
-                .pending("http://test.com/pending")
+                .success("http://yourwebsite.com/success")
+                .failure("http://yourwebsite.com/failure")
+                .pending("http://yourwebsite.com/pending")
                 .build()
         )
-        .differentialPricing(
-            PreferenceDifferentialPricingRequest.builder()
-                .id(1L)
-                .build()
-        )
-        .expires(false)
-        .items(items)
-        .marketplaceFee(BigDecimal("0"))
-        .payer(
-            PreferencePayerRequest.builder()
-                .name("Test")
-                .surname("User")
-                .email("your_test_email@example.com")
-                .phone(PhoneRequest.builder().areaCode("11").number("4444-4444").build())
-                .identification(
-                    IdentificationRequest.builder().type("CPF").number("19119119100").build()
-                )
-                .address(
-                    AddressRequest.builder()
-                        .zipCode("06233200")
-                        .streetName("Street")
-                        .streetNumber("123")
-                        .build()
-                )
-                .build()
-        )
-        .additionalInfo("Discount: 12.00")
-        .autoReturn("all")
-        .binaryMode(true)
-        .externalReference("1643827245")
-        .marketplace("marketplace")
-        .notificationUrl("http://notificationurl.com")
-        .operationType("regular_payment")
-        .paymentMethods(
-            PreferencePaymentMethodsRequest.builder()
-                .defaultPaymentMethodId("master")
-                .excludedPaymentTypes(excludedPaymentTypes)
-                .excludedPaymentMethods(excludedPaymentMethods)
-                .installments(5)
-                .defaultInstallments(1)
-                .build()
-        )
-        .shipments(
-            PreferenceShipmentsRequest.builder()
-                .mode("custom")
-                .localPickup(false)
-                .defaultShippingMethod(null)
-                .freeMethods(freeMethodList)
-                .cost(BigDecimal.TEN)
-                .freeShipping(false)
-                .dimensions("10x10x20,500")
-                .receiverAddress(
-                    PreferenceReceiverAddressRequest.builder()
-                        .zipCode("06000000")
-                        .streetNumber("123")
-                        .streetName("Street")
-                        .floor("12")
-                        .apartment("120A")
-                        .build()
-                )
-                .build()
-        )
-        .statementDescriptor("Test Store")
+        .autoReturn("approved")
         .build()
 
-    val preference = client.create(preferenceRequest)
-}
+    val preference: Preference = client.create(preferenceRequest)
 
+    fun printPaymentUrl() {
+        println("Payment URL: ${preference.initPoint}")  // URL para redirecionar o usuário para o Mercado Pago
+    }
+}
