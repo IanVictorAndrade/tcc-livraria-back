d3eb8274b26cce5ecc72661ca801ca8431b36968
ian <ianvictorandrade@hotmail.com>
2024-09-30 09:43:02 -0400

    wip

--- src/main/kotlin/com/ifro/tcc_livraria_back/dto/PreferenceItemDTO.kt
+++ src/main/kotlin/com/ifro/tcc_livraria_back/dto/PreferenceItemDTO.kt
@@ -3,10 +3,10 @@ package com.ifro.tcc_livraria_back.dto
 import java.math.BigDecimal
 
 data class PreferenceItemDTO (
-    val id: String?,
-    val title: String?,
+    val id: String,
+    val title: String,
     val description: String?,
-    val quantity: Int?,
-    val currencyId: String?,
-    val unitPrice: BigDecimal?
+    val quantity: Int,
+    val currencyId: String,
+    val unitPrice: BigDecimal
 )
